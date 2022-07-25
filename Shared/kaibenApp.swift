be0aa6abdcc45377af23a11d89700f1324d827b3
谷口功 <ko@13351095128.ap.cc.tsukuba.ac.jp>
2022-07-25 15:36:07 +0900

    first commit

---
+++ Shared/kaibenApp.swift
@@ -0,0 +1,17 @@
+//
+//  kaibenApp.swift
+//  Shared
+//
+//  Created by 谷口功 on 2022/07/25.
+//
+
+import SwiftUI
+
+@main
+struct kaibenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
