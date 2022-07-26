7d9f3d2f8319cf0c58fe30eeda00d1808358c636
TKmatsu2008 <fhenixeaglu1st@icloud.com>
2022-07-26 14:20:18 +0900

    6つのタイプで1つずつ食材を提案する #4

--- Shared/SuggestFoodView.swift
+++ Shared/SuggestFoodView.swift
@@ -9,51 +9,44 @@ import SwiftUI
 
 
 struct SuggestFoodView: View {
-    @State private var selectedIndex = 0
-
-    private let selectNames = ["バナナ", "コロコロ", "みみず", "どろどろ", "ビシャビシャ", "水、ころころ交互"]
+    let selectedIndex :Int=2
     var body: some View {
-        VStack {
-                
-                ForEach(0..<selectNames.count, id: \.self, content: { index in
-                    HStack {
-                        Text(selectNames[index])
-                // 解説1
-                        Image(systemName: selectedIndex == index ? "checkmark.circle.fill" : "circle")
-                            .foregroundColor(.blue)
-                    }
-                    .frame(height: 40)
-                // 解説2
-                    .onTapGesture {
-                        selectedIndex = index
-                    }
-                })
-                
-            }
         Text("あなたにおすすめの食材は")
         if selectedIndex == 0 {
             Text("龍郎")
                 .padding()
+            Image("banana")
+                .resizable()
         }
         else if selectedIndex == 1 {
             Text("水、バナナ")
                 .padding()
+            Image("banana")
+                .resizable()
         }
         else if selectedIndex == 2 {
             Text("バナナ")
                 .padding()
+            Image("banana")
+                .resizable()
         }
         else if selectedIndex == 3 {
             Text("ヨーグルト")
                 .padding()
+            Image("banana")
+                .resizable()
         }
         else if selectedIndex == 4 {
             Text("味噌汁")
                 .padding()
+            Image("banana")
+                .resizable()
         }
         else if selectedIndex == 5 {
             Text("納豆")
                 .padding()
+            Image("banana")
+                .resizable()
         }
     }
 }
